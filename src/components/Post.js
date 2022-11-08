fe751c1522f589ceab9f2759a97e401de4242838
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2022-11-08 16:51:39 -0300

    Primer commit

---
+++ src/components/Post.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+// El componente Post no tiene componentes hijos.
+// ESTADO: Post debe tener un número para almacenar la cantidad de likes, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Post debe tener un método para manejar el click de su boton, el cual debe:
+//  - sumar 1 like al posteo
+//  - sumar en la cantidad total de likes contabilizada por App
+//  - mostrar en consola qué posteo sumó un like
+// PROPS: Post recibe todos los campos que muestra en pantalla: titulo, texto, likes y el método heredados para sumar en el total.
+// Maqueta de Post:
+//    h3
+//    p
+//    button       (este boton debe permitir sumar likes)
+//    h5    (este h5 debe mostrar la cantidad de likes, pero si la misma es mayor a 10 debe decir "Más de 10 likes")
+
+export default function Post({item,aumentar}) {
+  const [likePost ,setLikePost] = useState(item.likes)
+  const aumentarPost=()=>{
+    aumentar()
+    setLikePost(likePost+1)
+    console.log("El posteo " + item.id + " sumo un like");
+  }
+  return (
+    <div className='posteo'>
+      <h3>{item.titulo}</h3>
+      <p>{item.texto}</p>
+      <button onClick={(aumentarPost)}>Like</button>
+      <h5>{likePost>10?<span> Mas de 10 likes </span>:likePost}</h5>
+    </div>
+  )
+}
